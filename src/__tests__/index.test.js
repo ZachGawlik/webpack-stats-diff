93fe5f966c3c42b3864c8a6b1d2dcca1b1dafd31
Zach Gawlik <ZachGawlik@users.noreply.github.com>
2018-05-05 18:27:38 -0400

    Create working basic api implementation

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,5 +1,79 @@
 const webpackStatsDiff = require('../index');
 
-test('returns empty object', () => {
-  expect(webpackStatsDiff({}, {})).toEqual({});
+test('handles blank inputs', () => {
+  expect(webpackStatsDiff([], [])).toEqual({
+    added: [],
+    removed: [],
+    bigger: [],
+    smaller: [],
+    sameSize: [],
+    total: {
+      newSize: 0,
+      oldSize: 0,
+      diff: 0
+    }
+  });
+});
+
+test('reports stat diffs between old and new assets', () => {
+  const oldAssets = [
+    { name: 'commons.js', size: 32768 },
+    { name: 'logo.svg', size: 588 },
+    { name: 'me.jpg', size: 1200000 },
+    { name: 'other-page.js', size: 40000 },
+    { name: 'main-site.js', size: 68000 }
+  ];
+  const newAssets = [
+    { name: 'commons.js', size: 65536 },
+    { name: 'Roboto-Regular.ttf', size: 176999 },
+    { name: 'me.jpg', size: 1200000 },
+    { name: 'other-page.js', size: 12345 },
+    { name: 'main-site.js', size: 38000 }
+  ];
+  expect(webpackStatsDiff(oldAssets, newAssets)).toEqual({
+    added: [
+      {
+        name: 'Roboto-Regular.ttf',
+        newSize: 176999,
+        oldSize: 0,
+        diff: 176999
+      }
+    ],
+    removed: [{ name: 'logo.svg', newSize: 0, oldSize: 588, diff: -588 }],
+    bigger: [
+      {
+        name: 'commons.js',
+        newSize: 65536,
+        oldSize: 32768,
+        diff: 32768
+      }
+    ],
+    smaller: [
+      {
+        name: 'other-page.js',
+        newSize: 12345,
+        oldSize: 40000,
+        diff: -27655
+      },
+      {
+        name: 'main-site.js',
+        newSize: 38000,
+        oldSize: 68000,
+        diff: -30000
+      }
+    ],
+    sameSize: [
+      {
+        name: 'me.jpg',
+        newSize: 1200000,
+        oldSize: 1200000,
+        diff: 0
+      }
+    ],
+    total: {
+      newSize: 1492880,
+      oldSize: 1341356,
+      diff: 151524
+    }
+  });
 });
--- src/index.js
+++ src/index.js
@@ -1,5 +1,74 @@
+const indexByName = assets => {
+  return assets.reduce((assetsByName, asset) => {
+    assetsByName[asset.name] = asset;
+    return assetsByName;
+  }, {});
+};
+
 const webpackStatsDiff = (oldAssets, newAssets) => {
-  return {};
+  const oldAssetsByName = indexByName(oldAssets);
+  const newAssetsByName = indexByName(newAssets);
+
+  const added = [];
+  const removed = [];
+  const bigger = [];
+  const smaller = [];
+  const sameSize = [];
+  let newSizeTotal = 0;
+  let oldSizeTotal = 0;
+
+  Object.keys(oldAssetsByName).forEach(name => {
+    const oldAsset = oldAssetsByName[name];
+    oldSizeTotal += oldAsset.size;
+    if (!newAssetsByName[name]) {
+      removed.push({
+        name,
+        newSize: 0,
+        oldSize: oldAsset.size,
+        diff: -1 * oldAsset.size
+      });
+    } else {
+      const diff = {
+        name,
+        newSize: newAssetsByName[name].size,
+        oldSize: oldAsset.size,
+        diff: newAssetsByName[name].size - oldAsset.size
+      };
+      if (diff.diff > 0) {
+        bigger.push(diff);
+      } else if (diff.diff < 0) {
+        smaller.push(diff);
+      } else {
+        sameSize.push(diff);
+      }
+    }
+  });
+
+  Object.keys(newAssetsByName).forEach(name => {
+    const newAsset = newAssetsByName[name];
+    newSizeTotal += newAsset.size;
+    if (!oldAssetsByName[newAsset.name]) {
+      added.push({
+        name,
+        newSize: newAsset.size,
+        oldSize: 0,
+        diff: newAsset.size
+      });
+    }
+  });
+
+  return {
+    added,
+    removed,
+    bigger,
+    smaller,
+    sameSize,
+    total: {
+      newSize: newSizeTotal,
+      oldSize: oldSizeTotal,
+      diff: newSizeTotal - oldSizeTotal
+    }
+  };
 };
 
 module.exports = webpackStatsDiff;
